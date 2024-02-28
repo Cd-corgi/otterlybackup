3cdc599e1ad341dc23387578cc8e293db8cc7821
Cd-corgi <systemjpc@hotmail.com>
2024-02-28 09:01:16 -0500

    adding style to ping.js

--- src/commands/misc/ping.js
+++ src/commands/misc/ping.js
@@ -3,15 +3,14 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('disc
 module.exports = {
     permissions: [PermissionFlagsBits.SendMessages],
     botp: [PermissionFlagsBits.SendMessages],
-    category: "main",
+    category: "misc",
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Test the ping response!"),
     async run(client, interaction) {
-        interaction.reply({ embeds: [
-            new EmbedBuilder()
-                .setTitle("Ping!")
-                .setColor("Random")
-        ] })
+        interaction.reply({ embeds: [new EmbedBuilder().setTitle("🌐 Ping Testing").setDescription(`Testing...`).setColor("Random")] })
+        setTimeout(() => {
+            interaction.editReply({ embeds: [new EmbedBuilder().setTitle(`🌐 Ping Testing!`).setDescription(`\`\`\`🤖 My Ping: ${Date.now() - interaction.createdTimestamp}ms\n😶 API: ${Math.round(client.ws.ping)}ms\`\`\``)] })
+        }, 3000);
     }
 }
